9f1b2de47f99f5f57bd87045bae8a7c2cacb409b
waltertick <ticktick.elvis@gmail.com>
2021-03-31 19:41:43 +0200

    feature of read more in toast message when user click on chevron icon

--- src/app/Shared/read-more/read-more.ts
+++ src/app/Shared/read-more/read-more.ts
@@ -10,7 +10,7 @@ import { Component, ViewEncapsulation, Input, AfterViewInit, ElementRef } from "
         <a (click)="isCollapsed = !isCollapsed" [hidden]="dataLength" class="pt-2 pull-right">
           <img  alt="Voir Plus" class="resizeImage" src="../assets/Images/VectorFleche.svg" *ngIf="isCollapsed">
 
-          <img  alt="Voir Plus" class="resizeImage" src="../assets/Images/VectorFleche2.svg" *ngIf="!isCollapsed">
+          <img  alt="Voir Plus" class="resizeImage" src="../assets/Images/VectorFleche1.svg" *ngIf="!isCollapsed">
         </a>
         <div class='cb'></div>
     `,
